0fd0212efb8b8f4ed1fe09aaa44086b12abfa74a
lokeshyerneni <lokeshyerneni0429@gmail.com>
2023-03-15 20:43:15 -0500

    Reads constraint file and variable files

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 import sys
-with open(sys.argv[1], 'r') as f:
-    contents = f.read()
-print(contents)
+
+# Initialized files, dictionary for var file
+varFile = open(sys.argv[1], 'r')
+varLines = varFile.readlines()
+varDict = {}
+
+# Appends numbers into dictionary for each value
+for line in varLines:
+    vals = line.split(":")
+    strListVal = list(line.split(":")[1].strip().replace(" ", ""))
+    varDict.update({vals[0]: [int(val) for val in strListVal]})
+
+conFile = open(sys.argv[2], 'r')
+conLines = conFile.readlines()
+conDict = []
+
+# Appends numbers into dictionary for each value
+for line in conLines:
+    conDict.append(line.strip())
+
+print(conDict)
