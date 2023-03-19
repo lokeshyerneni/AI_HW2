7a261bab3789d55c4f589c08aec235588710692e
ZzRanger <calebmarklim@gmail.com>
2023-03-18 19:31:41 -0500

    autoformat

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-# Command: py main.py ex1.var1.txt ex1.con.txt
+# Command: python main.py ex1.var1.txt ex1.con.txt
 import sys
 
 # Initialized files, dictionary for var file
-varFile = open(sys.argv[1], 'r')
+varFile = open(sys.argv[1], "r")
 varLines = varFile.readlines()
 varDict = {}
 varSize = 0
@@ -12,9 +12,9 @@ for line in varLines:
     vals = line.split(":")
     strListVal = list(line.split(":")[1].strip().replace(" ", ""))
     varDict.update({vals[0]: [int(val) for val in strListVal]})
-    varSize+=1
+    varSize += 1
 
-conFile = open(sys.argv[2], 'r')
+conFile = open(sys.argv[2], "r")
 conLines = conFile.readlines()
 conDict = []
 
@@ -23,56 +23,61 @@ for line in conLines:
     conDict.append(line.strip())
 
 forwardChecking = True
-if (sys.argv[3] == "none"):
+if sys.argv[3] == "none":
     forwardChecking = False
+
 
 def backtrackingSearch(csp, fc):
     return recursiveBacktracking({}, csp, fc)
 
+
 def selectUnassignedVariable(dict, assignment, csp):
     newKey = [key for key in dict if key not in assignment]
     return newKey[0]
+
 
 def checkConstraint(key, num, assignment, csp):
     assignment[key] = num
     constBool = True
     for con in csp:
         splitCon = con.split(" ")
-        
-        if (splitCon[0] in assignment and splitCon[2] in assignment):
-            if (splitCon[1] == ">"):
-                constBool = (assignment[splitCon[0]] > assignment[splitCon[2]])
-                if (not constBool):
+
+        if splitCon[0] in assignment and splitCon[2] in assignment:
+            if splitCon[1] == ">":
+                constBool = assignment[splitCon[0]] > assignment[splitCon[2]]
+                if not constBool:
                     return False
-            elif (splitCon[1] == "<"):
-                constBool = (assignment[splitCon[0]] < assignment[splitCon[2]])
-                if (not constBool):
+            elif splitCon[1] == "<":
+                constBool = assignment[splitCon[0]] < assignment[splitCon[2]]
+                if not constBool:
                     return False
-            elif (splitCon[1] == "="):
-                constBool = (assignment[splitCon[0]] == assignment[splitCon[2]])
-                if (not constBool):
+            elif splitCon[1] == "=":
+                constBool = assignment[splitCon[0]] == assignment[splitCon[2]]
+                if not constBool:
                     return False
-            elif (splitCon[1] == "!"):
-                constBool = (assignment[splitCon[0]] != assignment[splitCon[2]])
-                if (not constBool):
+            elif splitCon[1] == "!":
+                constBool = assignment[splitCon[0]] != assignment[splitCon[2]]
+                if not constBool:
                     return False
         else:
             continue
     return True
 
+
 def recursiveBacktracking(assignment, csp, fc):
     if len(assignment) == varSize:
         return assignment
-    
+
     var = selectUnassignedVariable(varDict, assignment, csp)
-    
+
     for num in varDict[var]:
-        if (checkConstraint(var, num, assignment, csp)):
+        if checkConstraint(var, num, assignment, csp):
             assignment[var] = num
             result = recursiveBacktracking(assignment, csp, fc)
-            if (result != "Failure"):
+            if result != "Failure":
                 return result
         del assignment[var]
     return "Failure"
+
 
 print(backtrackingSearch(conDict, forwardChecking))
